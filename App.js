a8fee9b3bc060c99b2cf7611c6717d9c124585be
Andrew Mok <andrewmmc@users.noreply.github.com>
2018-02-14 12:15:02 +0800

    ADD: Init

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import {Text} from 'react-native'
+import {Container, Header, Left, Body, Right, Button, Icon, Title} from 'native-base'
+
+export default class App extends Component {
+	render() {
+		return (
+			<Container>
+				<Header>
+					<Body>
+					<Title>Add9u</Title>
+					</Body>
+				</Header>
+				<Text>123</Text>
+			</Container>
+		)
+	}
+}
