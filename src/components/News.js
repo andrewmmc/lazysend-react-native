a2d6e928bef73c4b00b32a941f12da88dc2e935c
Andrew Mok <andrewmmc@users.noreply.github.com>
2018-02-26 16:32:53 +0800

    ADD: Add articles listview and axios for getting news

--- src/components/News.js
+++ src/components/News.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import React, {Component} from 'react'
 import {StyleSheet, Linking} from 'react-native'
 import {Container, Header, Content, List, ListItem, Thumbnail, Text, Body} from 'native-base'
@@ -15,20 +16,49 @@ export default class News extends Component {
 
 	constructor(props) {
 		super(props)
+		this.state = {
+			articles: [],
+			pageNum: 1
+		}
+	}
+
+	componentDidMount() {
+		this.getNews()
+	}
+
+	getNews() {
+		axios.post('https://us-central1-add9u-mobile.cloudfunctions.net/get-news', {
+			q: 'WhatsApp',
+			pageNum: this.state.pageNum
+		})
+			.then((response) => {
+				console.log(response)
+				if (response.data.status === 'ok') {
+					// this.state.articles.push(...response.data.articles)
+					this.setState({ articles: [...this.state.articles, ...response.data.articles] })
+				}
+			})
+			.catch((error) => {
+				console.log(error)
+			})
 	}
 
 	render() {
+		let articlesList = this.states.articles.map((item, index) => {
+			return <ListItem key={ index }>
+				<Thumbnail square size={ 80 } source={ { uri: item.urlToImage } }/>
+				<Body>
+				<Text>{ item.title }</Text>
+				<Text note>{ item.description }</Text>
+				</Body>
+			</ListItem>
+		})
+
 		return (
 			<Container style={ styles.pageContainer }>
 				<Content>
 					<List>
-						<ListItem>
-							<Thumbnail square size={ 80 } source={ { uri: '' } }/>
-							<Body>
-							<Text>Sankhadeep</Text>
-							<Text note>Its time to build a difference.</Text>
-							</Body>
-						</ListItem>
+						{ articlesList }
 					</List>
 				</Content>
 			</Container>
